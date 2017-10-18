9650c229a1cef638d95a569e37b3a677bb10ea6e
Ryan Lane TNW <r.i.lane@tudelft.nl>
2017-10-18 14:48:51 +0200

    functional for 2D dataset

--- super_stitch.py
+++ super_stitch.py
@@ -3,7 +3,7 @@
 @Author: rlane
 @Date:   10-10-2017 12:00:47
 @Last Modified by:   rlane
-@Last Modified time: 17-10-2017 18:45:59
+@Last Modified time: 18-10-2017 14:41:46
 """
 
 import os
@@ -15,7 +15,7 @@ import h5py
 
 from skimage.feature import ORB, match_descriptors
 from skimage.feature import register_translation
-from skimage.transform import SimilarityTransform, AffineTransform
+from skimage.transform import SimilarityTransform, AffineTransform, warp
 from skimage.graph import route_through_array
 from skimage.measure import label, ransac
 
@@ -142,7 +142,7 @@ def get_translations(data):
 
     # Accumulate translations
     cum_shifts = np.cumsum(shifts, axis=1)
-    cum_shifts[1,:,1] += 1500
+    # cum_shifts[1,:,1] += 1500
 
     translations = {}
     for row_keys, row_shifts in zip(keys, cum_shifts):
@@ -152,12 +152,10 @@ def get_translations(data):
     return translations
 
 
-def detect_features(img, **ORB_kws):
+def detect_features(img, ORB_kws=None):
     """
     """
-    default_ORB_kws = dict(downscale=1.2, n_scales=8, n_keypoints=800,
-                           fast_n=9, fast_threshold=0.05, harris_k=0.04)
-    ORB_kws = {**default_ORB_kws, **ORB_kws}
+    ORB_kws = {} if ORB_kws is None else ORB_kws
 
     orb = ORB(**ORB_kws)
 
@@ -168,53 +166,79 @@ def detect_features(img, **ORB_kws):
     return kps, dps
 
 
-def find_matches(img1, img2, **ORB_kws):
+def find_matches(img1, img2, ORB_kws=None):
     """
     """
-    kps_img1, dps_img1 = detect_features(img1, **ORB_kws)
-    kps_img2, dps_img2 = detect_features(img2, **ORB_kws)
+    kps_img1, dps_img1 = detect_features(img1, ORB_kws=ORB_kws)
+    kps_img2, dps_img2 = detect_features(img2, ORB_kws=ORB_kws)
 
     matches = match_descriptors(dps_img1, dps_img2, cross_check=True)
     return kps_img1, kps_img2, matches
 
 
+def estimate_transform(img1, img2, ORB_kws=None, ransac_kws=None):
+    """
+    """
+    kps_img1, kps_img2, matches = find_matches(img1, img2, ORB_kws=ORB_kws)
+    src = kps_img2[matches[:, 1]][:, ::-1]
+    dst = kps_img1[matches[:, 0]][:, ::-1]
+
+    ransac_kws = {} if ransac_kws is None else ransac_kws
+
+    model, inliers = ransac((src, dst), AffineTransform,
+                            min_samples=4, residual_threshold=1,
+                            **ransac_kws)
+    return model
+
+
 def get_translations_robust(data):
     """
-    Make this work
-    Have to run translation finding on 4-3, 5-3, ... etc (verticals)
     """
     img_dict, FM_imgs, EM_imgs, x_positions, y_positions = data
     shape = get_shape(data)
     keys = sort_keys(x_positions, y_positions, shape)
+    keys_T = np.array(keys).T.tolist()
 
-    shifts = []
+    ORB_kws = {'downscale': 2,
+               'n_keypoints': 800,
+               'fast_threshold': 0.05}
+    ransac_kws = {'max_trials': 600}
+
+    h_shifts = []
     for row in keys:
-        shifts.append(np.zeros(2))
+        h_shifts.append(np.zeros(2))
         for k1, k2 in zip(row, row[1:]):
 
-            kps_img1, kps_img2, matches = find_matches(
-                FM_imgs[k1], FM_imgs[k2])
+            model = estimate_transform(FM_imgs[k1], FM_imgs[k2],
+                                       ORB_kws=ORB_kws,
+                                       ransac_kws=ransac_kws)
 
-            src = kps_img2[matches[:, 1]][:, ::-1]
-            dst = kps_img1[matches[:, 0]][:, ::-1]
+            shift = model.translation
+            h_shifts.append(shift)
 
-            print(k1, k2)
+    v_shifts = []
+    for col in keys_T:
+        v_shifts.append(np.zeros(2))
+        for k1, k2 in zip(col, col[1:]):
 
-            model_robust, inliers = ransac(
-                (src, dst), AffineTransform, min_samples=4,
-                max_trials=300, residual_threshold=1)
+            model = estimate_transform(FM_imgs[k1], FM_imgs[k2],
+                                       ORB_kws=ORB_kws,
+                                       ransac_kws=ransac_kws)
 
-            shift = model_robust.translation
-            shifts.append(shift)
+            shift = model.translation
+            v_shifts.append(shift)
 
-    shifts = np.array(shifts).reshape(shape + (2,))
+    h_shifts = np.array(h_shifts).reshape(shape + (2,))
+    v_shifts = np.array(v_shifts).reshape(shape[::-1] + (2,))
 
-    # Accumulate translations
-    cum_shifts = np.cumsum(shifts, axis=1)
-    # cum_shifts[1,:,1] += 1500
+    cum_h_shifts = np.cumsum(h_shifts, axis=1)
+    cum_v_shifts = np.cumsum(v_shifts, axis=1)
+
+    tot_shifts = (cum_h_shifts.reshape(shape[-1]*2, 2) +
+                  cum_v_shifts.reshape(shape[-1]*2, 2)).reshape(shape + (2,))
 
     translations = {}
-    for row_keys, row_shifts in zip(keys, cum_shifts):
+    for row_keys, row_shifts in zip(keys, tot_shifts):
         for k, shift in zip(row_keys, row_shifts):
             translations[k] = SimilarityTransform(translation=shift)
 
@@ -230,9 +254,14 @@ def tile_images(data):
     img_dict, FM_imgs, EM_imgs, x_positions, y_positions = data
     keys = list(img_dict.keys())
 
-    translations = get_translations(data)
+    translations = get_translations_robust(data)
 
-    shifts = np.array([tr.translation for tr in translations.values()])
+    # import pickle
+    # with open('translations.pickle', 'rb') as handle:
+    #     translations = pickle.load(handle)
+
+    shifts = np.array([tr.translation for tr in translations.values()],
+                      dtype=np.int64)
 
     H_px, W_px = data[1][list(data[1].keys())[0]].shape
     output_shape = np.array([(shifts[:, 1].max() - shifts[:, 1].min()) + H_px,
