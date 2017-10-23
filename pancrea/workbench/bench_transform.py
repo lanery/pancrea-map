00a8f5f3015a0164a9c2799a2cb42248df27e4e9
Ryan Lane TNW <r.i.lane@tudelft.nl>
2017-10-23 15:37:57 +0200

    transform is model for benchtests

--- pancrea/workbench/bench_transform.py
+++ pancrea/workbench/bench_transform.py
@@ -1,61 +1,120 @@
 # -*- coding: utf-8 -*-
 # @Author: Ryan Lane
 # @Date:   2017-10-22 11:33:08
-# @Last Modified by:   Ryan Lane
-# @Last Modified time: 2017-10-23 08:12:56
+# @Last Modified by:   rlane
+# @Last Modified time: 23-10-2017 15:35:10
 
+import os
+import numpy as np
+import pandas as pd
+import time
 from tqdm import tqdm
 
 from .bench_utils import dict_product
 
 from ..odemis_data import load_data
-from ..stitch import get_keys, get_shape, find_matches
+from ..stitch import get_keys, get_shape, estimate_transform
 
-import logging
 
 
 def benchtest_transform(img1, img2):
     """
+    Runs benchmark tests for `stitch.estimate_transform`
+
+    Parameters
+    ----------
+    img1 : ndarray
+        Image data for first image
+    img2 : ndarray
+        Image data for seond image. Must have some overlap with img1 otherwise
+        this test is pointless and the transformation estimation will either
+        error or be bogus
+
+    Returns
+    -------
+    out : pd.DataFrame
+        Results of the benchmark test in the form of a pandas dataframe.
+        Results include parameters for ORB and RANSAC algorithms along
+        with the translations in x and y and runtime. If estimate transform
+        fails in any way, x and y translation are passed on as 0.
     """
+    # Parameters for ORB
     ORB_arg_dict = {
-        'downscale': [1.5, 2],
+        'downscale': [1.5, 2, 2.25, 2.5, 2.75, 3],
         'n_keypoints': [600, 800, 1000],
         'fast_threshold': 0.05
     }
 
+    # Parameters for RANSAC
     ransac_arg_dict = {
         'min_samples': [4],
         'residual_threshold': 1,
-        'max_trials': [600]
+        'max_trials': [600, 800, 1000]
     }
 
-    ORB_kws_list = dict_product(ORB_arg_dict)
-
+    # Get all possible combinations of parameter space
     ORB_kws_list = dict_product(ORB_arg_dict)
     ransac_kws_list = dict_product(ransac_arg_dict)
 
-    for ORB_kws in tqdm(ORB_kws_list, desc='ORB', ascii=True):
-        for ransac_kws in tqdm(ransac_kws_list, desc='RANSAC', ascii=True):
+    # Instantiate dataframe
+    df_out = pd.DataFrame()
 
-        # log.info('ORB parameters')
-        # log.info(ORB_kws)
+    ORB_desc = "Cycling through ORB Parameters"
+    ransac_desc = "Cylcing through RANSAC Parameters"
 
-        model = find_matches(img1, img2, ORB_kws=ORB_kws)
+    # Loop through ORB and RANSAC parameter sets to apply estimate_transform
+    for ORB_kws in tqdm(ORB_kws_list, desc=ORB_desc, ascii=True):
+        for ransac_kws in tqdm(ransac_kws_list, desc=ransac_desc, ascii=True):
+
+            params = {**ORB_kws, **ransac_kws}
+            start = time.clock()
+
+            try:
+                model = estimate_transform(
+                    img1, img2, ORB_kws=ORB_kws, ransac_kws=ransac_kws)
+                dX, dY = model.translation
+            except Exception:
+                dX, dY = (0, 0)
+
+            end = time.clock()
+            results = {
+                'dX': dX,
+                'dY': dY,
+                't': (end - start)
+            }
+
+            df_out = df_out.append({**params, **results}, ignore_index=True)
+
+    # Reorder dataframe columns so results come last
+    cols = list(params.keys()) + list(results.keys())
+    df_out = df_out.loc[:, cols]
+
+    return df_out
 
 
 if __name__ == '__main__':
 
-    logfile = 'pancrea//workbench//matchfinder.log'
-    logging.basicConfig(filename=logfile, filemode='w', level=logging.INFO)
-    log = logging.getLogger(__name__)
-
+    # Load data
     dir_name = 'sample_data//rat-pancreas'
     data = load_data(dir_name=dir_name)
-
     FM_imgs, EM_imgs, x_positions, y_positions = data
+    keys = get_keys(data)
+    shape = get_shape(data)
 
-    img1 = FM_imgs['tile_4-2']
-    img2 = FM_imgs['tile_4-3']
+    # Select random image pair
+    img_pairs = []
+    for row in keys:
+        img_pairs.append(list(zip(row, row[1:])))
+    img_pairs = np.array(img_pairs).reshape(shape[0] * (shape[1] - 1), 2)
+    rand_img_pair = img_pairs[np.random.randint(12)]
 
-    benchtest_matchfinder(img1, img2)
+    # Run bench test
+    df_out = benchtest_transform(FM_imgs[rand_img_pair[0]],
+                                 FM_imgs[rand_img_pair[1]])
 
+    # Save results to log file
+    logfile = 'pancrea//workbench//log_transform.log'
+    if os.path.exists(logfile):
+        df_out.to_csv(logfile, mode='a', header=False, index=False)
+    else:
+        df_out.to_csv(logfile, header=df_out.columns, index=False)
--- pancrea/workbench/bench_utils.py
+++ pancrea/workbench/bench_utils.py
@@ -3,7 +3,7 @@
 @Author: rlane
 @Date:   20-10-2017 10:39:06
 @Last Modified by:   rlane
-@Last Modified time: 20-10-2017 17:52:08
+@Last Modified time: 23-10-2017 15:33:33
 """
 
 from itertools import product
@@ -11,7 +11,40 @@ from itertools import product
 
 def dict_product(arg_dict):
     """
+    Take the Cartesian product of a dictionary of lists
+
+    Parameters
+    ----------
+    arg_dict : dict
+        Dictionary of lists or scalars
+        If a particular value within the dict is scalar, it will be converted
+        to a (length 1) list to make itertools product happy
+
+    Returns
+    -------
+    dict_list : list
+        Cartesian product of 
+
+    Notes
+    -----
+    https://stackoverflow.com/questions/5228158/cartesian-product-of-a-dictionary-of-lists
+
+    Examples
+    --------
+    >>> params = {'number': [1, 2, 3],
+                  'color': ['orange', 'blue']}
+
+    >>> dict_product(params)
+
+    [ {"number": 1, "color": "orange"},
+      {"number": 1, "color": "blue"},
+      {"number": 2, "color": "orange"},
+      {"number": 2, "color": "blue"},
+      {"number": 3, "color": "orange"},
+      {"number": 3, "color": "blue"}
+    ]
     """
+
     # Wrap all non-list dict values (floats/ints) in a list
     # so that they can be handled by product
     for k, v in arg_dict.items():
